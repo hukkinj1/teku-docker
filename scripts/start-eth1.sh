01bdd0e9754be429ad1bd902847adc1006665b0f
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-08-10 21:25:56 +0300

    Switch from besu to geth

--- scripts/start-eth1.sh
+++ scripts/start-eth1.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 #
 # Starts a local ETH1 node.
-
 if [ "$START_ETH1" != "" ]; then
-	exec besu \
-		--network=goerli \
-		--data-path=/opt/besu/data/data \
-		--host-allowlist=* \
-		--sync-mode=FAST \
-		--rpc-http-enabled \
-		--rpc-http-cors-origins=* \
-		--rpc-http-api=ETH
+	exec geth \
+		--goerli \
+		--http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth" \
+		--ipcdisable \
+		--nousb
 fi
