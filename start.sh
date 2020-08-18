4a69725c28da2070bf7b5cb63d053f207e11a9c1
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-08-18 17:09:12 +0300

    Create data dirs as non-root user

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
+#!/usr/bin/env bash
+
 export UID=${UID}
+export DATA_DIR_TEKU=./data_teku
+export DATA_DIR_ETH1=./data_eth1
+
+# Make sure data dirs are created without root being the owner
+mkdir -p ${DATA_DIR_TEKU}
+mkdir -p ${DATA_DIR_ETH1}
 
 docker-compose pull &&
 docker-compose down &&
